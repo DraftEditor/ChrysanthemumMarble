aec4d198903e985ed914af7993b66e68144e742f
Draft <45689230+DraftEditor@users.noreply.github.com>
2025-08-15 16:38:01 +0900

    Update ChrysanthemumMarble.user.js

--- dist/ChrysanthemumMarble.user.js
+++ dist/ChrysanthemumMarble.user.js
@@ -8,15 +8,15 @@
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://github.com/DraftEditor/ChrysanthemumMarble
 // @icon         https://raw.githubusercontent.com/DraftEditor/ChrysanthemumMarble/master/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com/gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com/gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.js
+// @updateURL    https://raw.githubusercontent.com/DraftEditor/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.js
+// @downloadURL  https://raw.githubusercontent.com/DraftEditor/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.js
 // @run-at       document-start
 // @match        *://*.wplace.live/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM.setValue
 // @grant        GM_getValue
-// @resource     CSS-BM-File https://raw.githubusercontent.com/gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/main/dist/ChrysanthemumMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/DraftEditor/ChrysanthemumMarble/refs/heads/main/dist/ChrysanthemumMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
